cee38ee3d6a7a1ea80a7efcdb6fe901e2bb4f01b
Shunsuke Kirino <skirino@flywheel.jp>
2023-04-09 02:50:33 +0900

    Update dev/test-only deps

--- mix.exs
+++ mix.exs
@@ -32,9 +32,9 @@ defmodule RaftKV.MixProject do
       {:croma       , "~> 0.9"},
       {:rafted_value, "~> 0.9"},
       {:raft_fleet  , "~> 0.8"},
-      {:dialyxir    , "~> 0.5" , [only: :dev ]},
-      {:ex_doc      , "~> 0.20", [only: :dev ]},
-      {:excoveralls , "~> 0.10", [only: :test]},
+      {:dialyxir    , "~> 1.3" , [only: :dev , runtime: false]},
+      {:ex_doc      , "~> 0.29", [only: :dev , runtime: false]},
+      {:excoveralls , "~> 0.16", [only: :test, runtime: false]},
     ]
   end
 
